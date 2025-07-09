ea79ae68f97be416a5d708fa66d8f067b633d21b
SeungHyeok Jang <inertia@SeungHyeoks-MacBook-Pro.local>
2025-07-09 16:55:11 +0900

    second stable version

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -47,11 +47,29 @@ class TIDE_LEVEL_15MIN_MULTI(Dataset):
 
     def __read_data__(self):
         self.scaler = StandardScaler()
-        df_raw = pd.read_csv(os.path.join(self.root_path,
-                                          self.data_path))
+        df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
 
-        border1s = [0, 12 * 30 * 24 * 4 - self.seq_len, 12 * 30 * 24 * 4 + 4 * 30 * 24 * 4 - self.seq_len]
-        border2s = [12 * 30 * 24 * 4, 12 * 30 * 24 * 4 + 4 * 30 * 24 * 4, 12 * 30 * 24 * 4 + 8 * 30 * 24 * 4]
+        # Dynamically calculate data split points
+        data_len = len(df_raw)
+        train_ratio = 0.7
+        val_ratio = 0.1
+        # test_ratio is implicitly 1 - train_ratio - val_ratio
+
+        train_len = int(data_len * train_ratio)
+        val_len = int(data_len * val_ratio)
+        test_len = data_len - train_len - val_len
+
+        border1s = [
+            0,
+            train_len - self.seq_len,
+            train_len + val_len - self.seq_len
+        ]
+        border2s = [
+            train_len,
+            train_len + val_len,
+            data_len
+        ]
+        
         border1 = border1s[self.set_type]
         border2 = border2s[self.set_type]
 
@@ -62,22 +80,23 @@ class TIDE_LEVEL_15MIN_MULTI(Dataset):
             df_data = df_raw[[self.target]]
 
         if self.scale:
-            train_data = df_data[border1s[0]:border2s[0]]
+            # Scaler is fit only on the training data
+            train_data = df_data.iloc[border1s[0]:border2s[0]]
             self.scaler.fit(train_data.values)
             data = self.scaler.transform(df_data.values)
         else:
             data = df_data.values
 
         df_stamp = df_raw[['date']][border1:border2]
-        df_stamp['date'] = pd.to_datetime(df_stamp.date)
+        df_stamp['date'] = pd.to_datetime(df_stamp['date'])
+        
         if self.timeenc == 0:
-            df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
-            df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
-            df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
-            df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
-            df_stamp['minute'] = df_stamp.date.apply(lambda row: row.minute, 1)
-            df_stamp['minute'] = df_stamp.minute.map(lambda x: x // 15)
-            data_stamp = df_stamp.drop(['date'], 1).values
+            df_stamp['month'] = df_stamp['date'].apply(lambda row: row.month)
+            df_stamp['day'] = df_stamp['date'].apply(lambda row: row.day)
+            df_stamp['weekday'] = df_stamp['date'].apply(lambda row: row.weekday())
+            df_stamp['hour'] = df_stamp['date'].apply(lambda row: row.hour)
+            df_stamp['minute'] = df_stamp['date'].apply(lambda row: row.minute // 15)
+            data_stamp = df_stamp.drop(columns=['date']).values
         elif self.timeenc == 1:
             data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
             data_stamp = data_stamp.transpose(1, 0)
